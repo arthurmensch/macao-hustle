3810f79f733a7081ee2bf6ddd8d0e172cca173a4
Arthur Mensch <arthur.mensch@gmail.com>
2015-01-06 00:47:10 +0100

    added figure generation

--- code/Macao.py
+++ code/Macao.py
@@ -8,39 +8,42 @@ mpl.use("pgf")
 pgf_with_pdflatex = {
 	"pgf.texsystem": "pdflatex",
 	"pgf.preamble": [
-		 r"\usepackage[utf8x]{inputenc}",
-		 r"\usepackage[T1]{fontenc}",
-		 r"\usepackage{amsmath}"
-		 ]
+		r"\usepackage[utf8x]{inputenc}",
+		r"\usepackage[T1]{fontenc}",
+		r"\usepackage{amsmath}"
+	]
 }
 mpl.rcParams.update(pgf_with_pdflatex)
 
 import matplotlib.pyplot as plt
-
+import pickle
 
 class Graph:
 	def __init__(self, cluster_size, r_mat):
 		self.cluster_size = cluster_size
 		self.num_cluster = np.size(self.cluster_size)
-		self.cluster_bounds = np.zeros(self.num_cluster+1,dtype=int)
-		self.cluster_bounds[1:self.num_cluster+1] = np.cumsum(cluster_size)
+		self.cluster_bounds = np.zeros(self.num_cluster + 1, dtype=int)
+		self.cluster_bounds[1:self.num_cluster + 1] = np.cumsum(cluster_size)
 		self.N = self.cluster_bounds[self.num_cluster]
 		self.nodes = np.arange(self.N)
 		self.C = []
-		for i in range(0,self.num_cluster):
-			self.C.append(self.nodes[self.cluster_bounds[i]:self.cluster_bounds[i+1]])
+		for i in range(0, self.num_cluster):
+			self.C.append(self.nodes[self.cluster_bounds[i]:self.cluster_bounds[i + 1]])
 		self.r_mat = r_mat
 
 	def find_cluster(self, I):
-		return np.searchsorted(self.cluster_bounds,I,side='right')-1
+		return np.searchsorted(self.cluster_bounds, I, side='right') - 1
 
 	def observe(self, I):
 		c = self.find_cluster(I)
 		obs = np.zeros(self.N)
-		for i in range(0,self.num_cluster):
-			obs[self.cluster_bounds[i]:self.cluster_bounds[i+1]] = np.random.choice(2, size=self.cluster_size[i], p =[1-self.r_mat[c,i], self.r_mat[c,i]])
+		for i in range(0, self.num_cluster):
+			obs[self.cluster_bounds[i]:self.cluster_bounds[i + 1]] = np.random.choice(2, size=self.cluster_size[i],
+																					  p=[1 - self.r_mat[c, i],
+																						 self.r_mat[c, i]])
 		obs[I] = 1
 		return obs
+
 
 class Adversary:
 	def __init__(self, game):
@@ -49,41 +52,44 @@ class Adversary:
 		self.T = game.T
 		N1 = int(self.N / 2)
 		N2 = self.N - N1
-		self.loss = np.hstack((np.random.uniform(5,10,size=[self.T,N1]),np.random.uniform(0,1,size=[self.T,N1])))
+		self.loss = np.hstack((np.random.uniform(5, 10, size=[self.T, N1]), np.random.uniform(0, 1, size=[self.T, N1])))
+
 
 class Game:
-	def __init__(self, graph, T, players_type,number):
+	def __init__(self, graph, T, players_type, number):
 		self.graph = graph
 		self.N = self.graph.N
 		self.T = T
 		self.number = number
 		self.players_type = players_type
 		self.num_players = len(self.players_type)
-		self.max_regret = np.zeros([self.T,self.num_players])
+		self.max_regret = np.zeros([self.T, self.num_players])
 
 
 	def init(self):
 		self.adversary = Adversary(self)
-		self.players =  [self.players_type[i](self) for i in range(0,self.num_players)]
+		self.players = [self.players_type[i](self) for i in range(0, self.num_players)]
 
 	def round(self):
 		observe = self.graph.observe
 		loss = self.adversary.loss
 		for player in self.players:
-			player.play(observe,loss)
+			player.play(observe, loss)
 
 	def run(self):
-		for i in range(0,self.number):
+		for i in range(0, self.number):
 			self.init()
-			for i in range(0,self.T):
+			for i in range(0, self.T):
 				self.round()
-			for j in range(0,self.num_players):
-				self.max_regret[:,j] += self.players[j].max_regret
+			for j in range(0, self.num_players):
+				self.max_regret[:, j] += self.players[j].max_regret
 		self.max_regret /= self.number
+		pickle.dump(self.max_regret,open('max_regret.p','wb+'))
 
 	def display(self):
-		for i in range(0,self.num_players):
-			plt.plot(np.arange(self.T),self.max_regret[:,i],label = self.players_type[i].playerName())
+		self.max_regret = pickle.load(open('max_regret.p','rb'))
+		for i in range(0, self.num_players):
+			plt.plot(np.arange(self.T), self.max_regret[:, i], label=self.players_type[i].playerName)
 		plt.legend(loc=2)
 		plt.savefig('regret.pdf')
 
@@ -97,136 +103,131 @@ class Player:
 		self.I = np.zeros(self.T)
 		self.max_regret = np.zeros(self.T)
 
-	def play(self,observe,loss):
-		I = self.choose_arm(observe,loss)
+	def play(self, observe, loss):
+		I = self.choose_arm(observe, loss)
 		self.I[self.t] = I
-		self.regret += loss[self.t,I] - loss[self.t]
-		self.max_regret[self.t]  = np.max(self.regret)
+		self.regret += loss[self.t, I] - loss[self.t]
+		self.max_regret[self.t] = np.max(self.regret)
 		self.t += 1
 
-	def choose_arm(self,observe,loss):
-		return np.random.randint(0,self.N)
+	def choose_arm(self, observe, loss):
+		return np.random.randint(0, self.N)
 
-	def __str__(self):
-		return 'Naive Player'
+	playerName = 'Naive Player'
 
-	def playerName():
-		return 'Naive Player'
 
 class DuplexpPlayer(Player):
-	def __init__(self,game):
-		Player.__init__(self,game)
+	def __init__(self, game):
+		Player.__init__(self, game)
 		self.graph = game.graph
 		Toff = self.T + 2
-		self.Lhat = np.zeros([Toff,self.N])
-		self.DLhat = np.zeros([Toff,self.N])
-		self.p = np.zeros([Toff,self.N])
-		self.O = np.zeros([Toff,self.N])
+		self.Lhat = np.zeros([Toff, self.N])
+		self.DLhat = np.zeros([Toff, self.N])
+		self.p = np.zeros([Toff, self.N])
+		self.O = np.zeros([Toff, self.N])
 		self.estimate_phase = False
-		self.last_t = np.zeros([2,self.graph.num_cluster])
+		self.last_t = np.zeros([2, self.graph.num_cluster])
 
-	def choose_arm(self,observe,loss):
+	def choose_arm(self, observe, loss):
 		if self.estimate_phase:
 			return super('choose_arm')
-			#TODO perform estimation
+		# TODO perform estimation
 		else:
 			K = np.zeros(self.N)
 			G = np.zeros(self.N)
 			t = self.t
 			e = t % 2
-			eta = np.sqrt(np.log(self.N)/((self.N*self.N)
-						  + np.sum(np.multiply(np.multiply(self.p[e:t:2],self.DLhat[e:t:2]),self.DLhat[e:t:2]))))
-			w = -eta*self.Lhat[t-2,:]
+			eta = np.sqrt(np.log(self.N) / ((self.N * self.N)
+											+ np.sum(
+				np.multiply(np.multiply(self.p[e:t:2], self.DLhat[e:t:2]), self.DLhat[e:t:2]))))
+			w = -eta * self.Lhat[t - 2, :]
 			w = w - np.max(w)
 			w = np.exp(w)
-			self.p[t,:] = w/np.sum(w)
-			I = np.random.choice(int(self.N),p=self.p[t,:])
-			self.O[t,:] = observe(I)
+			self.p[t, :] = w / np.sum(w)
+			I = np.random.choice(int(self.N), p=self.p[t, :])
+			self.O[t, :] = observe(I)
 			c = self.graph.find_cluster(I)
 			M = np.zeros(self.graph.num_cluster)
-			for k in range(0,self.graph.num_cluster):
-				Oc = self.O[self.last_t[e,c],self.graph.C[k]]
+			for k in range(0, self.graph.num_cluster):
+				Oc = self.O[self.last_t[e, c], self.graph.C[k]]
 				if k == c:
-					I_last = self.I[self.last_t[e,c]]
-					Oc = np.delete(Oc,I_last-self.graph.cluster_bounds[self.graph.find_cluster(I_last)])
+					I_last = self.I[self.last_t[e, c]]
+					Oc = np.delete(Oc, I_last - self.graph.cluster_bounds[self.graph.find_cluster(I_last)])
 				min_i = np.nonzero(Oc)[0]
-				M[k] = min_i[0] if np.size(min_i) > 0 else self.graph.cluster_size[k]-1
-			self.last_t[e,c] = t
+				M[k] = min_i[0] if np.size(min_i) > 0 else self.graph.cluster_size[k] - 1
+			self.last_t[e, c] = t
 			G = np.zeros(self.N)
-			for i in range(0,self.N):
-				c= self.graph.find_cluster(i)
-				K = np.random.geometric(self.p[t,i])
-				G[i] = min(M[c],K)
-			lhat = np.multiply(np.multiply(loss[t],self.O[t,:]),G)
-			self.DLhat[t,:] = lhat
-			self.Lhat[t,:] = self.Lhat[t-2,:]+self.DLhat[t,:]
+			for i in range(0, self.N):
+				c = self.graph.find_cluster(i)
+				K = np.random.geometric(self.p[t, i])
+				G[i] = min(M[c], K)
+			lhat = np.multiply(np.multiply(loss[t], self.O[t, :]), G)
+			self.DLhat[t, :] = lhat
+			self.Lhat[t, :] = self.Lhat[t - 2, :] + self.DLhat[t, :]
 			return I
 
-	def __str__(self):
-		return 'Duplexp'
+	playerName = 'Duplex SBM'
 
-	def playerName():
-		return 'Duplexp SBM'
+
 
 class DuplexpPlayerErdos(Player):
-	def __init__(self,game):
-		Player.__init__(self,game)
+	def __init__(self, game):
+		Player.__init__(self, game)
 		Toff = self.T + 2
-		self.Lhat = np.zeros([Toff,self.N])
-		self.DLhat = np.zeros([Toff,self.N])
-		self.p = np.zeros([Toff,self.N])
-		self.O = np.zeros([Toff,self.N])
+		self.Lhat = np.zeros([Toff, self.N])
+		self.DLhat = np.zeros([Toff, self.N])
+		self.p = np.zeros([Toff, self.N])
+		self.O = np.zeros([Toff, self.N])
 		self.estimate_phase = False
 		self.last_t = np.zeros(2)
 
 
-	def choose_arm(self,observe,loss):
+	def choose_arm(self, observe, loss):
 		if self.estimate_phase:
 			return super('choose_arm')
-			#TODO perform estimation
+		# TODO perform estimation
 		else:
 			K = np.zeros(self.N)
 			G = np.zeros(self.N)
 			t = self.t
 			e = t % 2
-			eta = np.sqrt(np.log(self.N)/((self.N*self.N)
-						  + np.sum(np.multiply(np.multiply(self.p[e:t:2],self.DLhat[e:t:2]),self.DLhat[e:t:2]))))
-			w = -eta*self.Lhat[t-2,:]
+			eta = np.sqrt(np.log(self.N) / ((self.N * self.N)
+											+ np.sum(
+				np.multiply(np.multiply(self.p[e:t:2], self.DLhat[e:t:2]), self.DLhat[e:t:2]))))
+			w = -eta * self.Lhat[t - 2, :]
 			w = w - np.max(w)
 			w = np.exp(w)
-			self.p[t,:] = w/np.sum(w)
-			I = np.random.choice(int(self.N),p=self.p[t,:])
-			self.O[t,:] = observe(I)
-			Oc = self.O[self.last_t[e],:]
+			self.p[t, :] = w / np.sum(w)
+			I = np.random.choice(int(self.N), p=self.p[t, :])
+			self.O[t, :] = observe(I)
+			Oc = self.O[self.last_t[e], :]
 			I_last = self.I[self.last_t[e]]
-			Oc = np.delete(Oc,I_last)
+			Oc = np.delete(Oc, I_last)
 			min_i = np.nonzero(Oc)[0]
-			M = min_i[0] if np.size(min_i) > 0 else self.N-1
+			M = min_i[0] if np.size(min_i) > 0 else self.N - 1
 			self.last_t[e] = t
 			G = np.zeros(self.N)
-			for i in range(0,self.N):
-				K = np.random.geometric(self.p[t,i])
-				G[i] = min(M,K)
-			lhat = np.multiply(np.multiply(loss[t,:],self.O[t,:]),G)
-			self.DLhat[t,:] = lhat
-			self.Lhat[t,:] = self.Lhat[t-2,:]+self.DLhat[t,:]
+			for i in range(0, self.N):
+				K = np.random.geometric(self.p[t, i])
+				G[i] = min(M, K)
+			lhat = np.multiply(np.multiply(loss[t, :], self.O[t, :]), G)
+			self.DLhat[t, :] = lhat
+			self.Lhat[t, :] = self.Lhat[t - 2, :] + self.DLhat[t, :]
 			return I
 
-	def __str__(self):
-		return 'Duplexp Erdös'
+	playerName = 'Duplex Erdös'
 
-	def playerName():
-		return 'Duplexp Erdös'
 
 def test():
 	T = 10
-	num = 50
-	graph = Graph([200,200,200],np.array([[0.1,0.05,0],[0.05,0.2,0],[0,0,0.5]]))
-	#graph = Graph([600],np.array([[0.2]]))
-	players_type = [DuplexpPlayer,DuplexpPlayerErdos,Player]
-	game = Game(graph,T,players_type,num)
+	num = 5
+	graph = Graph([200, 200, 200], np.array([[0.1, 0.05, 0], [0.05, 0.2, 0], [0, 0, 0.5]]))
+	# graph = Graph([600],np.array([[0.2]]))
+	players_type = [DuplexpPlayer, DuplexpPlayerErdos, Player]
+	game = Game(graph, T, players_type, num)
 	game.run()
 	game.display()
+
 
 if __name__ == '__main__':
 	test()
