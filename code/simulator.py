b59b139b25e380bb88ee729cbf8601a10657f6cf
Arthur Mensch <arthur.mensch@gmail.com>
2015-01-05 11:12:40 +0100

    notation

--- code/simulator.py
+++ code/simulator.py
@@ -4,17 +4,32 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-N = 1000
-T = 1000
-r = 0.01
+N1 = 1000
+N2 = 1000
+N = N1 + N2
+C1 = np.arange(N1)
+C2 = np.arange(N2)+N1
+T = 5000
+r11 = 0.1
+r22 = 0.2
+r12 = 0.05
+r21 = 0.05
 
 def loss():
-	return np.random.uniform(0,1,size=[N])
+	return np.hstack((np.random.uniform(4,8,size=[200]),np.random.uniform(0,1,size=[800])))
 	#return np.arange(N)
 
 def observed(I):
-	rand = np.random.choice(2, size=N, p =[1-r, r])
-	rand[I] = 1
+	rand = np.zeros(N)
+	if I in C1:
+		rand[C1] = np.random.choice(2, size=N1, p =[1-r11, r11])
+		rand[I] = 1
+		rand[C2] = np.random.choice(2, size=N2, p =[1-r12, r12])
+	else:
+		rand[C2] = np.random.choice(2, size=N2, p =[1-r12, r12])
+		rand[I] = 1
+		rand[C1] = np.random.choice(2, size=N1, p =[1-r21, r12])
+
 	return rand, np.nonzero(rand)[0]
 
 def play_naive():
@@ -33,16 +48,17 @@ def play_naive():
 def play_duplex():
 	rbar,tstart,max_regret_start = estimate_r()
 	regret = max_regret_start[tstart-2]
-	Toff = T+2-tstart
-
-	thres = np.log(Toff)/(2*N)
+	thres = np.log(T)/(2*N)
 
 	if rbar == 0:
 		return exp3()
 	elif rbar < thres:
-		A = np.int(np.ceil(np.log(Toff)/(N*rbar)))
+		A = np.int(np.ceil(np.log(T)/(N*rbar)))
 	else:
 		A = 1
+
+	Toff = T+1+A-tstart
+
 
 
 	Lhat = np.zeros([Toff,N])
@@ -68,7 +84,7 @@ def play_duplex():
 	I_naive = np.zeros(Toff)
 
 	#round = np.int(Toff / A)
-	for t in range(2,Toff+1-A,A):
+	for t in range(1+A,Toff+1-A,A):
 		e = t % 2
 		eta[t] = np.sqrt(np.log(N)/(A*A*N*N) + np.sum(np.multiply(p[e:t:2],DLhat[e:t:2],DLhat[e:t:2])))
 		w[t,:] = 1/N * np.exp(-eta[t]*Lhat[t-2,:])
@@ -80,7 +96,7 @@ def play_duplex():
 			I[t+j] = np.random.choice(N,p=p[t,:])
 			O[t+j,:], O_index = observed(I[t+j])
 			observed_loss.append(np.hstack((O_index, l[t+j,O_index])))
-			Od = np.nonzero(np.delete(O[t+j,:],I[t+j]))
+			Od = np.nonzero(np.delete(O[t-A+j,:],I[t-A+j]))
 			if np.size(Od[0]) >0:
 				M[t+j] = Od[0][0]
 			else:
@@ -97,9 +113,72 @@ def play_duplex():
 		DLhat[t,:] = np.sum(lhat[t:t+A,:],axis=0)
 		Lhat[t,:] = Lhat[t-2,:]+DLhat[t,:]
 
-	max_regret = np.hstack((max_regret_start,max_regret[2:]))
+	max_regret = np.hstack((max_regret_start,max_regret[1+A:]))
 	plt.plot(np.arange(np.size(max_regret)),max_regret)
 	plt.show()
+
+def play_duplex_simple():
+	Toff = T+2
+
+	Lhat = np.zeros([Toff,N])
+	DLhat = np.zeros([Toff,N])
+	O = np.zeros([Toff,N])
+	eta = np.zeros([Toff,N])
+	p = np.zeros([Toff,N])
+	l = np.zeros([Toff,N])
+	lhat = np.zeros([Toff,N])
+	w = np.zeros([Toff,N])
+	W = np.zeros(Toff)
+	I = np.zeros([Toff])
+	observed_loss = []
+
+	M = np.zeros([Toff])
+	K = np.zeros([Toff,N])
+	G = np.zeros([Toff,N])
+
+	regret = np.zeros(N)
+	regret_naive = np.zeros(N)
+	max_regret = np.zeros(Toff)
+	max_regret_naive = np.zeros(Toff)
+	I_naive = np.zeros(Toff)
+
+	last_t1  = 0
+	last_t2 = 0
+
+	#round = np.int(Toff / A)
+	for t in range(2,Toff):
+		e = t % 2
+		eta[t] = np.sqrt(np.log(N)/(N*N) + np.sum(np.multiply(p[e:t:2],DLhat[e:t:2],DLhat[e:t:2])))
+		w[t,:] = 1/N * np.exp(-eta[t]*Lhat[t-2,:])
+		W[t] = np.sum(w[t,:])
+		p[t,:] = w[t,:]/ W[t]
+
+		l[t,:] = loss()
+		I[t] = np.random.choice(N,p=p[t,:])
+		O[t,:], O_index = observed(I[t])
+		observed_loss.append(np.hstack((O_index, l[t,O_index])))
+		Od = np.nonzero(np.delete(O[t-1,:],I[t-1]))
+		if np.size(Od[0]) >0:
+			M[t] = Od[0][0]
+		else:
+			M[t] = N-1
+		for i in range(0,N):
+			K[t,i] = np.random.geometric(p[t,i])
+			G[t,i] = min(M[t],K[t,i])
+		lhat[t,:] = np.multiply(l[t,:],O[t,:],G[t,:])
+		regret += l[t,I[t]]-l[t,:]
+		max_regret[t] = np.max(regret)
+		I_naive[t] = np.random.randint(0,1000)
+		regret_naive += l[t,I_naive[t]]-l[t+j,:]
+		max_regret_naive[t] = np.max(regret_naive)
+		DLhat[t,:] = lhat[t,:]
+		Lhat[t,:] = Lhat[t-2,:]+DLhat[t,:]
+
+	max_regret = np.hstack((max_regret_start,max_regret[1+A:]))
+	plt.plot(np.arange(np.size(max_regret)),max_regret)
+	plt.show()
+
+
 
 def estimate_r():
 	k = np.int(np.ceil(math.e * np.log(T)/2))
@@ -131,7 +210,6 @@ def estimate_r():
 				if j == k:
 					return 1/(np.max(M)+1),t+1,np.array(max_regret)
 		return 0,T,np.array(max_regret)
-	#TODO
 
 if __name__ == '__main__':
 	play_duplex()
